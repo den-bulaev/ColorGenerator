92afab81406257e7c8f46b426f7a9e0756742d92
den-bulaev <denisbulaev7@gmail.com>
2024-08-12 14:50:14 +0300

    strict mode reverted

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "react-toastify/dist/ReactToastify.css";
@@ -6,4 +7,8 @@ import App from "./App.tsx";
 
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
